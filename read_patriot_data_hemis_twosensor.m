fcc451792a202c0a9f363e4f543c9522f2b027cb
subhro101 <36718433+subhro101@users.noreply.github.com>
2018-11-05 00:24:06 -0500

    Update read_patriot_data_hemis_twosensor.m

--- read_patriot_data_hemis_twosensor.m
+++ read_patriot_data_hemis_twosensor.m
@@ -35,7 +35,7 @@ angular_velocity = [];
 
 for i = 0:ntrackable-1
 
-    %% TA's notes: get position and orientation from the patriot.csv file. No need to modify.
+   
     pos = M(2:end, (3 + i*7):(5 + i*7))' / 100; % convert from centimeter to meter
     ori = M(2:end, (6 + i*7):(8 + i*7))' * pi / 180; % convert from degree to radians
     
@@ -43,8 +43,6 @@ for i = 0:ntrackable-1
     pos_all_trackable{i+1} = zeros(3, nframe);
   n=0;  
     for f = 1:nframe
-        %% TA's Notes: the function 'euler2rotm' takes the orientation data from the patriot.csv file
-        %%      and converts it into the rotation matrix based on Eq. 2.65 in the textbook. 
         
         n=n+1;    
         [R,D] = euler2rotm(ori(:, f)); % rotation matrix in patriot frame
@@ -53,34 +51,23 @@ for i = 0:ntrackable-1
         else
             R = Rn * R;
         end 
-        %% TA's Notes: the position data (x,y,z) is taken from the file and put into a vector form here:
+        
         d = pos(:, f);
 
-        %% TA's Notes: the rotation R and the position d are taken together to form a homogeneous transformation matrix 
-        %%  (based on Eq. 2.91 in the textbook)
+       
         A2 = [R            d;
               zeros(1, 3)   1];
 
-        %% TA's Notes: you'll have to do some coordinate system transformation here to get it with respect of the tool.
-
-        %% Question 6 a) done here.6c You should also calculate the linear and angular velocities here since you
-        %%  will have access to the (x,y,z) and orientation.
        
         skew = D * transpose(R);
         ang =[  skew(1, end) skew(2) skew(end, 2)       ];
         angular_velocity=[angular_velocity; ang];
         %% -- perform your functions here before inputting it into patriot_frame2_to_0_transfm(...) function.
 
-        %% TA's Notes: the Patriot's frame is different to that of the viewer. This part 
-        %%      changes its coordinate system to that of the interface.
-        %% -- remember that A0 and A2 are homogeneous transformation matrices. You will need to compute inverse of it 
-        %%      in a different way than usual.
+       
         A0 = patriot_frame2_to_0_transfm(A2);
 
-        %% TA's notes: use the angular velocity equation to obtain the skew matrix and then take the values for x, y, z:
-        %%      You can probably save everything into a matrix and use it to generate plots.
-        %% ang = ......; 
-        %% angular_velocity = [angular_velocity; ang];
+        
         
         A0_all_trackable{i+1}{f} = A0;
         pos_all_trackable{i+1}(:, f) = A0(1:3, 4);
@@ -90,8 +77,7 @@ for i = 0:ntrackable-1
 end
 
 
-%% TA's Notes: the position and orientation stays within the scope of this file. You should perform your operations here
-%%      while you have easier access to it; otherwise, you will need to access the data stored within cell structures 'A0_all_trackable'.
+
 linear_velocity = [];
 linear_velocity = [];
 %% velocity = pos_x - pos_x-1 / tendlinear_velocity = [linear_velocity; temp;];end
@@ -154,5 +140,3 @@ ylabel('y')
 zlabel('z')
 
 plot(x,y);
-%% TA's Notes: Try using 3D plots such as scatter3, comet3, etc. for those which require 3D visualization.
-%%      Otherwise, use regular subplot/plot for anything that can be visualized in 1D/2D.
